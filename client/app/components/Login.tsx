302a79024fc7dd515e70592bb58fe2343eb15a45
Ahmed Abdulaal <ahmedabdulaal746@gmail.com>
2023-11-11 09:25:08 +0300

    google login button

--- client/app/components/Login.tsx
+++ client/app/components/Login.tsx
@@ -9,10 +9,11 @@ import {
 } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import { FaGoogle } from 'react-icons/fa'
 
 export default function Login() {
     return (
-        <Card className="w-96 bg-white bg-opacity-10 text-white shadow-md">
+        <Card className="w-96 bg-white bg-opacity-10 text-white shadow-lg">
             <CardHeader>
             <CardTitle>تسجيل الدخول</CardTitle>
             <CardDescription className="text-gray-400">فضلا ادخل بياناتك</CardDescription>
@@ -32,8 +33,15 @@ export default function Login() {
             </form>
             </CardContent>
             <CardFooter className="flex justify-between">
-                <Button className="w-full bg-cbtn">دخول</Button>
+                <Button className="w-full bg-cbtn shadow-lg">دخول</Button>
             </CardFooter>
+            <div className="flex flex-col justify-center items-center">
+            <div className="mx-auto my-4 flex w-full items-center justify-evenly before:mr-4 before:block before:h-px before:flex-grow before:bg-stone-400 after:ml-4 after:block after:h-px after:flex-grow after:bg-stone-400">
+                    او باستخدام
+            </div>
+                <Button className="w-[300px] bg-cbtn m-3 shadow-lg flex gap-2">Google<FaGoogle/></Button>  
+            </div>
         </Card>
+
     )
 }
