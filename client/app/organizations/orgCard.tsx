85c4a7600dba561fa200f1483efe9804bccdfb77
JaafarAlMuallim <Ja3far03@gmail.com>
2023-11-15 07:06:52 +0300

    added org model and organization route and sub route

---
+++ client/app/organizations/orgCard.tsx
@@ -0,0 +1,28 @@
+import Organization from "@/models/org";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function OrgCard(props: { org: Organization }) {
+  return (
+    <Link href={`/organizations/${props.org.id}`}>
+      <div className="my-5 mx-5 shadow-xl px-10 rounded-lg bg-slate-800 py-10">
+        <Image
+          src={props.org.image!}
+          alt=""
+          className="rounded my-10"
+          width={320}
+          height={320}
+        />
+        <div className="my-2 text-xl font-semibold text-right text-purple-500">
+          <p>{props.org.founding_date}</p>
+        </div>
+        <div className="my-3 text-2xl font-semibold text-right text-white">
+          <p>{props.org.name}</p>
+        </div>
+        <div className="my-5 text-xl font-semibold text-right text-white">
+          <p className="">{props.org.description}</p>
+        </div>
+      </div>
+    </Link>
+  );
+}
