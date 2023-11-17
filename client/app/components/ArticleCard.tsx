5b47dbc649b10124d3f6e02049764f3080f6fb2b
Ahmed Abdulaal <ahmedabdulaal746@gmail.com>
2023-11-17 22:48:45 +0300

    edited article page to use the new article card

--- client/app/components/ArticleCard.tsx
+++ client/app/components/ArticleCard.tsx
@@ -6,16 +6,26 @@ import { GoHeartFill } from "react-icons/go"
 export default function ArticleCard(props: { article: Article }) {
     return (
         <Link href={`/articles/${props.article.id}`}>
-            <div className="block w-80 rounded-lg bg-cover shadow-lg bg-[url(/test.jpg)]">
-                <div className="w-full h-full flex items-center justify-between p-8 backdrop-brightness-50 rounded-lg">
-                    <Label className="mb-2 text-lg font-bold leading-tight text-white dark:text-neutral-50 shadwo-xl">
-                        {props.article.title}
-                    </Label>
-                    <div className="flex flex-col justify-center items-center">
-                        <GoHeartFill />{props.article.likes}
+            <div className="block w-80 text-white rounded-lg bg-cover shadow-lg bg-[url(/test.jpg)]">
+                <div className="flex flex-col backdrop-brightness-50 rounded-lg">
+                    <div className="w-full flex items-center justify-between p-6">
+                        <Label className="mb-1 text-lg font-bold leading-tight text-white shadwo-xl">
+                            {props.article.title}
+                        </Label>
+                        <div className="flex flex-col justify-center items-center">
+                            <GoHeartFill />{props.article.likes}
+                        </div>
                     </div>
-                    
-                </div>  
+                    <div className="my-2">
+                        {props.article.tags!.map((tag, index) => {
+                            return (
+                                <div key={index} className='bg-gray-600 bg-opacity-75 px-2 mx-1 rounded-full inline text-center text-xs font-semibold'>
+                                    {tag}
+                                </div>
+                            );
+                            })}
+                    </div>
+                </div>
             </div>
         </Link>
     )
