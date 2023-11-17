b0a052d784a2920c1cc751eaeb6d2812571fdcbf
Ahmed Abdulaal <ahmedabdulaal746@gmail.com>
2023-11-17 10:26:03 +0300

    modified cards to a responsive grid

--- client/app/about-us/page.tsx
+++ client/app/about-us/page.tsx
@@ -28,7 +28,7 @@ const AboutUsPage = () => {
       </div>
     </section><div className="text-center p-4">
         <div className="text-4xl text-white">فريق العمل</div>
-        <div className="flex flex-col gap-4 items-center">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6">
           <UserCard
             iconPath="/bukha.png"
             name="أحمد بوخمسين"
