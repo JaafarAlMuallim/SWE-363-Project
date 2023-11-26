9666ecdae602cc09b434e245c8d9ceeac757ae93
JaafarAlMuallim <Ja3far03@gmail.com>
2023-11-26 10:25:31 +0300

    updated controllers and routes for profile

--- server/src/controllers/profile.ts
+++ server/src/controllers/profile.ts
@@ -1,169 +1,120 @@
 import db from "../db";
-import { eq, and, or } from "drizzle-orm";
+import { eq } from "drizzle-orm";
 import { NextFunction, Request, Response } from "express";
-import { User, users, user_prefs, prefs, UserData, orgs } from "../schema";
-import { userInfo } from "os";
-
-type UserProfileType = Omit<User, "user_id">;
+import { users } from "../schema";
 
 export async function getUserProfile(
   req: Request,
   res: Response,
-  next: NextFunction
-) {
-  const user = await db.query.users.findFirst({
-    where: eq(users.user_id, req.params.id),
-
-    // depends on giving the id in params or in body
-  });
-  const userData: UserData = {
-    name: req.body.name,
-    role: "user",
-    user_id: "",
-    username: "",
-    email: "",
-    password: "",
-    verified: false,
-    x_account: "",
-    linkdin_account: "",
-    website: "",
-    user_image: "",
-  };
-  res.send(user);
-}
-export async function getAdminProfile(
-  req: Request,
-  res: Response,
-  next: NextFunction
-) {
-  const user = await db.query.users.findFirst({
-    where: eq(users.user_id, req.params.id),
-
-    // depends on giving the id in params or in body
-  });
-  const userData: UserData = {
-    name: req.body.name,
-    role: "admin",
-    user_id: "",
-    username: "",
-    email: "",
-    password: "",
-    verified: false,
-    x_account: "",
-    linkdin_account: "",
-    website: "",
-    user_image: "",
-  };
-  res.send(user);
-}
-export async function getReviewerProfile(
-  req: Request,
-  res: Response,
-  next: NextFunction
-) {
-  const user = await db.query.users.findFirst({
-    where: eq(users.user_id, req.params.id),
-
-    // depends on giving the id in params or in body
-  });
-  const userData: UserData = {
-    name: req.body.name,
-    role: "reviewer",
-    user_id: "",
-    username: "",
-    email: "",
-    password: "",
-    verified: false,
-    x_account: "",
-    linkdin_account: "",
-    website: "",
-    user_image: "",
-  };
-  res.send(user);
-}
-export async function updateAdminProfile(
-  req: Request,
-  res: Response,
-  next: NextFunction
+  next: NextFunction,
 ) {
   try {
-    const userData: UserData = {
-      name: req.body.name,
-      role: "admin",
-      user_id: "",
-      username: "",
-      email: "",
-      password: "",
-      verified: false,
-      x_account: "",
-      linkdin_account: "",
-      website: "",
-      user_image: "",
-    };
-    // Update the user profile
-    const updatedUser = await db
-      .update(users)
-      .set(userData)
-      .where(eq(users.user_id, req.params.id));
-    res.send(updatedUser);
+    const user = await db.query.users.findFirst({
+      where: eq(users.user_id, req.params.id),
+    });
+    res.send(user);
   } catch (e) {
     next(e);
   }
 }
+
 export async function updateUserProfile(
   req: Request,
   res: Response,
-  next: NextFunction
+  next: NextFunction,
 ) {
   try {
-    const userData: UserData = {
-      name: req.body.name,
-      role: "user",
-      user_id: "",
-      username: "",
-      email: "",
-      password: "",
-      verified: false,
-      x_account: "",
-      linkdin_account: "",
-      website: "",
-      user_image: "",
-    };
-    // Update the user profile
-    const updatedUser = await db
+    const user = await db
       .update(users)
-      .set(userData)
-      .where(eq(users.user_id, req.params.id));
-    res.send(updatedUser);
+      .set({
+        name: req.body.name,
+        email: req.body.email,
+        username: req.body.username,
+        role: req.body.role,
+        verified: req.body.verified,
+        x_account: req.body.x_account,
+        linkdin_account: req.body.linkdin_account,
+        website: req.body.website,
+        user_image: req.body.user_image,
+      })
+      .where(eq(users.user_id, req.params.id))
+      .returning();
+    res.send(user);
   } catch (e) {
     next(e);
   }
 }
-export async function updateReveiwerProfile(
+
+export async function updateUserProfileImage(
   req: Request,
   res: Response,
-  next: NextFunction
+  next: NextFunction,
 ) {
   try {
-    const userData: UserData = {
-      name: req.body.name,
-      role: "reviewer",
-      user_id: "",
-      username: "",
-      email: "",
-      password: "",
-      verified: false,
-      x_account: "",
-      linkdin_account: "",
-      website: "",
-      user_image: "",
-    };
-    // Update the user profile
-    const updatedUser = await db
+    const user = await db
       .update(users)
-      .set(userData)
-      .where(eq(users.user_id, req.params.id));
-    res.send(updatedUser);
+      .set({
+        user_image: req.body.user_image,
+      })
+      .where(eq(users.user_id, req.params.id))
+      .returning();
+    res.send(user);
+  } catch (e) {
+    next(e);
+  }
+}
+
+export async function updateUserProfilePassword(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  try {
+    const user = await db
+      .update(users)
+      .set({
+        password: req.body.password,
+      })
+      .where(eq(users.user_id, req.params.id))
+      .returning();
+    res.send(user);
+  } catch (e) {
+    next(e);
+  }
+}
+export async function updateVerifiedStatus(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  try {
+    const user = await db
+      .update(users)
+      .set({
+        verified: req.body.verified,
+      })
+      .where(eq(users.user_id, req.params.id))
+      .returning();
+    res.send(user);
+  } catch (e) {
+    next(e);
+  }
+}
+
+export async function updateRole(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  try {
+    const user = await db
+      .update(users)
+      .set({
+        role: req.body.role,
+      })
+      .where(eq(users.user_id, req.params.id))
+      .returning();
+    res.send(user);
   } catch (e) {
     next(e);
   }
