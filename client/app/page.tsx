946e7722e3c50e6f87d34328cf5fad47748e72e6
ItsJustM7 <mohammed54268@gmail.com>
2023-12-11 19:00:01 +0300

    onClick navegating button

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -9,6 +9,12 @@ import {
 } from "@/components/ui/Accordion";
 import Link from "next/link";
 export default function Home() {
+  const navigateToSection = (sectionId: string) => {
+    const section = document.getElementById(sectionId);
+    if (section) {
+      section.scrollIntoView({ behavior: "smooth" });
+    }
+  };
   return (
     <>
       <div className="flex flex-col items-center justify-center text-content">
@@ -37,53 +43,59 @@ export default function Home() {
               انضم لنا
             </Button>
           </Link>
-          <Link href="/articles">
-            <Button className="bg-cbtn text-content shadow-lg my-10 w-50">
-              اعرف المزيد
-            </Button>
-          </Link>
+
+          <Button
+            className="bg-cbtn text-content shadow-lg my-10 w-50"
+            onClick={() => navigateToSection("section2")}
+          >
+            اعرف المزيد
+          </Button>
         </div>
 
         <div>
-          <p className=" content-center mb-5 ml-5 mr-2">
-            مرحبًا بك في منصتنا المبتكرة المخصصة للشركات الناشئة، حيث تتحقق
-            الأحلام الريادية. إليك لماذا يجب عليك اختيارنا للحصول على رؤى فريدة،
-            وتوجيه شامل، ودعم لا مثيل له
-          </p>
-          <Accordion className="  mb-10 ml-2 mr-1" type="single" collapsible>
-            <AccordionItem value="item-1">
-              <AccordionTrigger>تركيز خاص على الشركات الناشئة</AccordionTrigger>
-              <AccordionContent>
-                نختص بشكل حصري في مجتمع الشركات الناشئة، مما يضمن أن يكون
-                محتوانا مركزًا بشكل كامل على التحديات، والفرص، والاتجاهات التي
-                تهم أكثر للأعمال الناشئة.
-              </AccordionContent>
-            </AccordionItem>
-            <AccordionItem value="item-2">
-              <AccordionTrigger>مركز موارد شامل</AccordionTrigger>
-              <AccordionContent>
-                يعتبر موقعنا مركزًا شاملاً للموارد، حيث يوفر مجموعة غنية من
-                المقالات والدلائل والأدوات التي تغطي كل جانب من جوانب رحلة
-                الشركة الناشئة - من الفكرة إلى التوسع.
-              </AccordionContent>
-            </AccordionItem>
-            <AccordionItem value="item-3">
-              <AccordionTrigger>تحليلات ورؤى من قبل الخبراء</AccordionTrigger>
-              <AccordionContent>
-                استفد من حكمة رواد الأعمال ذوي الخبرة، والخبراء في الصناعة،
-                ورؤوس الأفكار الذين يسهمون بمحتوى حصري، مما يوفر لك آراءً قيمة
-                واستراتيجيات للنجاح.
-              </AccordionContent>
-            </AccordionItem>
-            <AccordionItem value="item-4">
-              <AccordionTrigger>الاتصال بالمجتمع</AccordionTrigger>
-              <AccordionContent>
-                انضم إلى مجتمع نابض بالحياة من عشاق ريادة الأعمال. شارك في
-                المناقشات، وشارك التجارب، وقم ببناء اتصالات ذات مغزى مع رواد
-                الأعمال الآخرين والمرشدين والمستثمرين.
-              </AccordionContent>
-            </AccordionItem>
-          </Accordion>
+          <section id="section2">
+            <p className=" content-center mb-5 ml-5 mr-2">
+              مرحبًا بك في منصتنا المبتكرة المخصصة للشركات الناشئة، حيث تتحقق
+              الأحلام الريادية. إليك لماذا يجب عليك اختيارنا للحصول على رؤى
+              فريدة، وتوجيه شامل، ودعم لا مثيل له
+            </p>
+            <Accordion className="  mb-10 ml-2 mr-1" type="single" collapsible>
+              <AccordionItem value="item-1">
+                <AccordionTrigger>
+                  تركيز خاص على الشركات الناشئة
+                </AccordionTrigger>
+                <AccordionContent>
+                  نختص بشكل حصري في مجتمع الشركات الناشئة، مما يضمن أن يكون
+                  محتوانا مركزًا بشكل كامل على التحديات، والفرص، والاتجاهات التي
+                  تهم أكثر للأعمال الناشئة.
+                </AccordionContent>
+              </AccordionItem>
+              <AccordionItem value="item-2">
+                <AccordionTrigger>مركز موارد شامل</AccordionTrigger>
+                <AccordionContent>
+                  يعتبر موقعنا مركزًا شاملاً للموارد، حيث يوفر مجموعة غنية من
+                  المقالات والدلائل والأدوات التي تغطي كل جانب من جوانب رحلة
+                  الشركة الناشئة - من الفكرة إلى التوسع.
+                </AccordionContent>
+              </AccordionItem>
+              <AccordionItem value="item-3">
+                <AccordionTrigger>تحليلات ورؤى من قبل الخبراء</AccordionTrigger>
+                <AccordionContent>
+                  استفد من حكمة رواد الأعمال ذوي الخبرة، والخبراء في الصناعة،
+                  ورؤوس الأفكار الذين يسهمون بمحتوى حصري، مما يوفر لك آراءً قيمة
+                  واستراتيجيات للنجاح.
+                </AccordionContent>
+              </AccordionItem>
+              <AccordionItem value="item-4">
+                <AccordionTrigger>الاتصال بالمجتمع</AccordionTrigger>
+                <AccordionContent>
+                  انضم إلى مجتمع نابض بالحياة من عشاق ريادة الأعمال. شارك في
+                  المناقشات، وشارك التجارب، وقم ببناء اتصالات ذات مغزى مع رواد
+                  الأعمال الآخرين والمرشدين والمستثمرين.
+                </AccordionContent>
+              </AccordionItem>
+            </Accordion>
+          </section>
         </div>
       </div>
     </>
